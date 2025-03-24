799068a3afb1ebaa21ccd2e93b17777c531c55ee
ludovikallen <ludovikallen@gmail.com>
2025-03-24 16:29:49 -0400

    Add post install of Electron and launch script (#7)

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
+import { exec } from 'child_process';
+import path from 'path';
 import { UserConfigFn } from 'vite';
 import { overrideVaadinConfig } from './vite.generated';
 
-const customConfig: UserConfigFn = () => ({
-    // Here you can add custom Vite parameters
-    // https://vitejs.dev/config/
-});
+const customConfig: UserConfigFn = (configEnv) => {
+    if (configEnv.command === 'serve' && process.env.NODE_ENV === 'development') {
+        const electronPath = path.join(process.cwd(), 'node_modules', '.bin', 'electron');
+
+        exec(electronPath + ' .');
+    }
+
+    return {};
+};
 
 export default overrideVaadinConfig(customConfig);
